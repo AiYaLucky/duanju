bd38c0dce41e3f45a69c5f0c57cfa09bc0522b56
aiyalucky <aiyalucky@126.com>
2022-10-20 15:08:56 +0800

    删除无用的变量

--- app/src/main/java/com/shortvideo/ui/LoginActivity.java
+++ app/src/main/java/com/shortvideo/ui/LoginActivity.java
@@ -3,6 +3,8 @@ package com.shortvideo.ui;
 
 import android.os.Bundle;
 
+import androidx.lifecycle.ViewModelProviders;
+
 import com.shortvideo.BR;
 import com.shortvideo.R;
 import com.shortvideo.databinding.ActivityLoginBinding;
@@ -20,5 +22,11 @@ public class LoginActivity extends BaseActivity<ActivityLoginBinding, LoginViewM
     @Override
     public int initVariableId() {
         return BR.viewModel;
+    }
+
+    @Override
+    public LoginViewModel initViewModel() {
+        //View持有ViewModel的引用，如果没有特殊业务处理，这个方法可以不重写
+        return ViewModelProviders.of(this).get(LoginViewModel.class);
     }
 }

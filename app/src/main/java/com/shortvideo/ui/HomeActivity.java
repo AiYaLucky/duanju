0411a9a208555365a4de538d7cd48b07dba0a650
aiyalucky <aiyalucky@126.com>
2022-10-23 02:00:36 +0800

    主页底部菜单添加

--- app/src/main/java/com/shortvideo/ui/HomeActivity.java
+++ app/src/main/java/com/shortvideo/ui/HomeActivity.java
@@ -2,6 +2,9 @@ package com.shortvideo.ui;
 
 import android.os.Bundle;
 
+import androidx.lifecycle.ViewModelProviders;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.shortvideo.BR;
 import com.shortvideo.R;
 import com.shortvideo.databinding.ActivityHomeBinding;
@@ -10,14 +13,39 @@ import com.shortvideo.viewmodel.HomeViewModel;
 import me.goldze.mvvmhabit.base.BaseActivity;
 
 public class HomeActivity extends BaseActivity<ActivityHomeBinding, HomeViewModel> {
-
     @Override
     public int initContentView(Bundle savedInstanceState) {
         return R.layout.activity_home;
     }
 
     @Override
+    public void initData() {
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        navView.setOnNavigationItemSelectedListener(onNavigationItemSelected);
+    }
+
+    @Override
     public int initVariableId() {
         return BR.viewModel;
     }
+
+    @Override
+    public HomeViewModel initViewModel() {
+        return ViewModelProviders.of(this).get(HomeViewModel.class);
+    }
+
+    public BottomNavigationView.OnNavigationItemSelectedListener onNavigationItemSelected = item -> {
+        switch (item.getItemId()) {
+            case R.id.home:
+                System.out.println(1);
+                return true;
+            case R.id.favorite:
+                System.out.println(2);
+                return true;
+            case R.id.mine:
+                System.out.println(3);
+                return true;
+        }
+        return false;
+    };
 }
--- app/src/main/java/com/shortvideo/viewmodel/HomeViewModel.java
+++ app/src/main/java/com/shortvideo/viewmodel/HomeViewModel.java
@@ -3,6 +3,8 @@ package com.shortvideo.viewmodel;
 import android.app.Application;
 
 import androidx.annotation.NonNull;
+import androidx.databinding.ObservableField;
+import androidx.databinding.adapters.AdapterViewBindingAdapter;
 
 import me.goldze.mvvmhabit.base.BaseModel;
 import me.goldze.mvvmhabit.base.BaseViewModel;
@@ -14,6 +16,8 @@ import me.goldze.mvvmhabit.base.BaseViewModel;
  * Created by IntelliJ IDEA.
  */
 public class HomeViewModel extends BaseViewModel<BaseModel> {
+    private ObservableField<AdapterViewBindingAdapter.OnItemSelected> mNavigationView = new ObservableField<>();
+
     public HomeViewModel(@NonNull Application application) {
         super(application);
     }

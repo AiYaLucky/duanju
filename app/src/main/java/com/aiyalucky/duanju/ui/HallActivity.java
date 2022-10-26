eff1f10086f71aef3f064b8e5376a09561b9e9ed
aiyalucky <aiyalucky@126.com>
2022-10-26 22:27:53 +0800

    修改提交

---
+++ app/src/main/java/com/aiyalucky/duanju/ui/HallActivity.java
@@ -0,0 +1,16 @@
+package com.aiyalucky.duanju.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.aiyalucky.duanju.R;
+
+public class HallActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hall);
+    }
+}
